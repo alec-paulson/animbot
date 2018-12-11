a058b937fb71683ba7be85da75f23b70ef03339d
Alec Paulson <alec.paulson@vantiv.com>
2018-12-11 10:45:34 -0700

    Express endpoint hello world

--- index.js
+++ index.js
@@ -12,3 +12,12 @@ var server = http.createServer(function onRequest (req, res) {
  
 // Listen
 server.listen(3000)
+
+//Express
+const express = require('express')
+const app = express()
+const port = 3001
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
