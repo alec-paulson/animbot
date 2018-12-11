90e59743db483494d708476afd908483dc00cd80
Alec Paulson <alec.paulson@vantiv.com>
2018-12-11 15:42:23 -0700

    Updated demo questions

--- public/js/index.js
+++ public/js/index.js
@@ -32,15 +32,7 @@ $(document).on('click', '.icon_close', function (e) {
 $(document).on('click', '#btn-chat', function(e) {
     $("#chat-panel").append('<div class="row msg_container base_sent">    <div class="col-xs-10 col-md-10">        <div class="messages msg_sent">            <p>' + $('#btn-input').val() + '</p>            <time datetime="2009-11-13T20:00">Arjun</time>        </div>    </div>    <div class="col-md-2 col-xs-2 avatar">        <img src="images/lead.jpg" class=" img-responsive ">    </div></div>');
     $("#btn-input").val('');
-<<<<<<< HEAD
     ajaxCall("http://localhost:3001/Answer", "POST", "{'Answer' : 'Fire in the hole'}")
-    //$('#chat-panel').animate({ 
-    //    scrollTop: $(document).height()-$(window).height()}, 
-    //    1400, 
-    //    "swing"
-    // );
-=======
->>>>>>> 184e86a331dbe659473e524d9bd65d750051c2de
     $('#chat-panel').animate({scrollTop:$(document).height()}, 'swing');
 });
 
